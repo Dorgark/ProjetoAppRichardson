d22ad8bf4fbfc247e2716083e4c371d02ac605af
Dorgark <caiquebpa@gmail.com>
2024-08-25 22:07:47 -0300

    BANCO DE DADOS CORRIGDO
    faltava a extensao do arquivo".cs"

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265c63449a1cbdc58f0952dc7420fe8190296448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4475dbce94fa128ae237a11144cab535fcf9389")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
